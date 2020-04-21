ac98a12a2560ece54a23bb7833e5ec17fa18dc7e
NebulaFX <62919604+NebulaFX@users.noreply.github.com>
2020-04-21 20:32:14 +0100

    Delete info.py

--- info.py
+++
@@ -1,7 +0,0 @@
-#  [*] Type your username and password for addon Below
-#  [*] If you don't want to type it in here, the script will not work.
-#  [*] This file is called by info.py, so keep it in the same folder
-#usernamegrab, passwordgrab
-
-usernamegrab = 'USERNAME'
-passwordgrab = 'PASSWORD'
